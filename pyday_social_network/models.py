0fc985f4f3ee4a422ba8486fc627defa4566bcde
6desislava6 <desislavatsvetkova@mail.bg>
2016-05-02 21:59:33 +0300

    Validation of email

--- pyday_social_network/models.py
+++ pyday_social_network/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.timezone import now
+from django.core.validators import validate_email
 
 
 class PyDayManager(BaseUserManager):
@@ -11,15 +12,23 @@ class PyDayManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, password=password, **extra_fields)
         user.set_password(password)
+        user.clean_fields()
         user.save()
+        return user
 
     def create_user(self, email, password=None, **extra_fields):
         return self._create_user(email, password, **extra_fields)
 
+    def create_user_request(self, request):
+        self.create_user(first_name=request.POST['first_name'],
+                         last_name=request.POST['last_name'],
+                         email=request.POST['email'],
+                         picture=request.FILES['file'])
+
 
 class PyDayUser(AbstractBaseUser):
-    email = models.CharField(max_length=200, unique=True, default='')
-    # password = models.CharField(max_length=200, default='')
+    email = models.EmailField(max_length=200, unique=True, default='',
+                              validators=[validate_email])
     first_name = models.CharField(max_length=100, default='')
     last_name = models.CharField(max_length=100, default='')
     picture = models.FileField(upload_to='pictures/')
--- pyday_social_network/views.py
+++ pyday_social_network/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from pyday_social_network.forms import UploadPictureForm
 from pyday_social_network.models import PyDayUser
+from django.core.exceptions import ValidationError
 # from django.http import HttpResponseRedirect
 
 
@@ -9,11 +10,12 @@ def upload_picture(request):
     if request.method == 'POST':
         form = UploadPictureForm(request.POST, request.FILES)
         if form.is_valid():
-            PyDayUser.objects.create_user(first_name=request.POST['first_name'],
-                                  last_name=request.POST['last_name'],
-                                  email=request.POST['email'],
-                                  picture=request.FILES['file'])
-            return HttpResponse('стаа!')
+            try:
+                PyDayUser.objects.create_user_request(request)
+            except ValidationError:
+                return HttpResponse('невалиден мейл, бре')
+            else:
+                return HttpResponse('стаа!')
 
         else:
             return HttpResponse('не стаа')
